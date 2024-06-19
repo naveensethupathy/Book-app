403b89da53ed50f3288e6e5832521a5a323355d8
naveensethupathy <naveensethupathy1325@gmail.com>
2024-06-19 17:56:42 +0530

    This is a new commit for what I originally planned to be amended

--- backend/server.js
+++ backend/server.js
@@ -17,6 +17,17 @@ app.use(express.json())
 app.use(express.urlencoded({extended: false}))
 app.use('/api/books',require('./routes/bookroutes'))
 app.use('/api/words',require('./routes/wordroutes'))
-
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '/frontend/build')))
+  
+    app.get('*', (req, res) =>
+      res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
+    )
+}
+else{
+    app.get('/',(req,res)=>{
+        res.send("Api is running");
+    })
+}
 
 app.listen(port, ()=> console.log(`Server running on port ${port}`));
